57e961f4bc4e76e38275fe2080a5558b633581a0
jmccutchen <purplegrits@yahoo.com>
2023-11-15 06:21:38 -0500

    need to fix SVC run, it crashes

--- wine_quality.py
+++ wine_quality.py
@@ -83,73 +83,73 @@ print('Totals after cleaning: \n', df.shape)
 
 warnings.filterwarnings('ignore')
 
-# show histplot for each column
-plt.figure(figsize=(15, 10))
-plot_num = 1
-for i in df.columns:
-    ax = plt.subplot(4, 4, plot_num)
-    sns.histplot(df[i], kde=True)
-    plot_num += 1
-plt.tight_layout()
-plt.show()
+# # show histplot for each column
+# plt.figure(figsize=(15, 10))
+# plot_num = 1
+# for i in df.columns:
+#     ax = plt.subplot(4, 4, plot_num)
+#     sns.histplot(df[i], kde=True)
+#     plot_num += 1
+# plt.tight_layout()
+# plt.show()
 
-# correlation map
-# -1 means: There is a negative relationship between dependent and independent variables .
-# 0 means: There is no relationship between dependent and independent variables .
-# 1 means: There is a positive relationship between dependent and independent variables .
-# According to this information, it can be made a good analysis about dataset and columns.
-df.corr()
-f, ax = plt.subplots(figsize=(10, 10))
-sns.heatmap(df.corr(), annot=True, linewidths=.5, fmt=".2f", ax=ax)
-plt.show()
+# # correlation map
+# # -1 means: There is a negative relationship between dependent and independent variables .
+# # 0 means: There is no relationship between dependent and independent variables .
+# # 1 means: There is a positive relationship between dependent and independent variables .
+# # According to this information, it can be made a good analysis about dataset and columns.
+# df.corr()
+# f, ax = plt.subplots(figsize=(10, 10))
+# sns.heatmap(df.corr(), annot=True, linewidths=.5, fmt=".2f", ax=ax)
+# plt.show()
 
-# show multivariate analysis
-sns.pairplot(df, hue='quality', height=3)
-plt.tight_layout()
-plt.show()
+# # show multivariate analysis
+# sns.pairplot(df, hue='quality', height=3)
+# plt.tight_layout()
+# plt.show()
 
-# show scatterplot analysis
-fig, axes = plt.subplots(11, 11, figsize=(25, 25))
-for i in range(11):
-    for j in range(11):
-        axes[i, j].scatter(df.iloc[:, i], df.iloc[:, j], c=df.quality)
-        axes[i, j].set_xlabel(df.columns[i])
-        axes[i, j].set_ylabel(df.columns[j])
-        axes[i, j].legend(df.quality)
-plt.tight_layout()
-plt.show()
+# # show scatterplot analysis
+# fig, axes = plt.subplots(11, 11, figsize=(25, 25))
+# for i in range(11):
+#     for j in range(11):
+#         axes[i, j].scatter(df.iloc[:, i], df.iloc[:, j], c=df.quality)
+#         axes[i, j].set_xlabel(df.columns[i])
+#         axes[i, j].set_ylabel(df.columns[j])
+#         axes[i, j].legend(df.quality)
+# plt.tight_layout()
+# plt.show()
 
-g = sns.pairplot(df, hue="quality")
+# g = sns.pairplot(df, hue="quality")
 
-# distribution of quality rankings
-sns.barplot(x=df['quality'].unique(), y=df['quality'].value_counts())
-plt.xlabel("Quality Rankings")
-plt.ylabel("Number of Red Wine")
-plt.title("Distribution of Red Wine Quality Ratings")
-plt.show()
+# # distribution of quality rankings
+# sns.barplot(x=df['quality'].unique(), y=df['quality'].value_counts())
+# plt.xlabel("Quality Rankings")
+# plt.ylabel("Number of Wine")
+# plt.title("Distribution of Wine Quality Ratings")
+# plt.show()
 
 
-# Check the outliers for each feature with respect to output value
-fig, ax1 = plt.subplots(4, 3, figsize=(22, 16))
-k = 0
-for i in range(4):
-    for j in range(3):
-        if k != 12:
-            sns.barplot(y=df.iloc[:, k], x='quality',
-                        data=df, ax=ax1[i][j])
-            k += 1
-plt.tight_layout()
-plt.show()
+# # Check the outliers for each feature with respect to output value
+# fig, ax1 = plt.subplots(4, 3, figsize=(22, 16))
+# k = 0
+# for i in range(4):
+#     for j in range(3):
+#         if k != 12:
+#             sns.barplot(y=df.iloc[:, k], x='quality',
+#                         data=df, ax=ax1[i][j])
+#             k += 1
+# plt.tight_layout()
+# plt.show()
 
-# Check the outliers for each feature with respect to output value
-fig, ax1 = plt.subplots(4, 3, figsize=(22, 16))
-k = 0
-for i in range(4):
-    for j in range(3):
-        if k != 11:
-            sns.boxplot(x='quality', y=df.iloc[:, k], data=df, ax=ax1[i][j])
-            k += 1
-plt.show()
+# # Check the outliers for each feature with respect to output value
+# fig, ax1 = plt.subplots(4, 3, figsize=(22, 16))
+# k = 0
+# for i in range(4):
+#     for j in range(3):
+#         if k != 11:
+#             sns.boxplot(x='quality', y=df.iloc[:, k], data=df, ax=ax1[i][j])
+#             k += 1
+# plt.show()
 
 ##################################  Models- Classification and Cross Validation ##################################
 
@@ -215,6 +215,9 @@ def normalization(X):
 def train_test(X_t, y):
     x_train, x_test, y_train, y_test = train_test_split(
         X_t, y, test_size=0.3, random_state=42)
+    sc = StandardScaler()
+    x_train = sc.fit_transform(x_train)
+    x_test = sc.fit_transform(x_test)
     print("Train:", len(x_train), " - Test:", len(x_test))
     return x_train, x_test, y_train, y_test
 
@@ -358,14 +361,17 @@ print("X_t:", X_t.shape)
 x_train, x_test, y_train, y_test = train_test(X_t, y)
 
 
-lr = LogisticRegression()
-apply_classification('Logistic_Regression', lr,
-                     x_train, x_test, y_train, y_test)
+# lr = LogisticRegression()
+# print('running LR classification')
+# apply_classification('Logistic_Regression', lr,
+#                      x_train, x_test, y_train, y_test)
 
-svm = SVC()
+svm = SVC(gamma='auto')
+print('running SVM classification')
 apply_classification('SVM', svm, x_train, x_test, y_train, y_test)
 
 dt = DecisionTreeClassifier()
+print('running DT classification')
 dt_clf = apply_classification(
     'Decision_Tree', dt, x_train, x_test, y_train, y_test)
 
@@ -376,152 +382,153 @@ graph = graphviz.Source(dot_data)
 graph
 
 rf = RandomForestClassifier(n_estimators=100)
+print('running RF classification')
 apply_classification('Random_Forest', rf, x_train, x_test, y_train, y_test)
 
 ##### Binary dataset analysis ##############################################
 # Add a new feature according to mean of the quality
 # Good wine represented by 1, bad wine represented by 0
-df['value'] = ""
-df['value'] = [1 if each > 5 else 0 for each in df['quality']]
+# df['value'] = ""
+# df['value'] = [1 if each > 5 else 0 for each in df['quality']]
 
-print("Good Wine Class:", df[df['value'] == 1].shape)
-print("Bad Wine Class:", df[df['value'] == 0].shape)
+# print("Good Wine Class:", df[df['value'] == 1].shape)
+# print("Bad Wine Class:", df[df['value'] == 0].shape)
 
-# Check the outliers for each feature with respect to output value
-fig, ax1 = plt.subplots(4, 3, figsize=(22, 16))
-k = 0
-for i in range(4):
-    for j in range(3):
-        if k != 11:
-            sns.boxplot(x='value', y=df.iloc[:, k], data=df, ax=ax1[i][j])
-            k += 1
-plt.show()
+# # Check the outliers for each feature with respect to output value
+# fig, ax1 = plt.subplots(4, 3, figsize=(22, 16))
+# k = 0
+# for i in range(4):
+#     for j in range(3):
+#         if k != 11:
+#             sns.boxplot(x='value', y=df.iloc[:, k], data=df, ax=ax1[i][j])
+#             k += 1
+# plt.show()
 
-# Categorical distribution plots:
-fig, ax1 = plt.subplots(4, 3, figsize=(22, 16))
-k = 0
-for i in range(4):
-    for j in range(3):
-        if k != 11:
-            sns.barplot(x="value", y=df.iloc[:, k],
-                        hue='value', data=df, ax=ax1[i][j])
-            k += 1
-plt.show()
+# # Categorical distribution plots:
+# fig, ax1 = plt.subplots(4, 3, figsize=(22, 16))
+# k = 0
+# for i in range(4):
+#     for j in range(3):
+#         if k != 11:
+#             sns.barplot(x="value", y=df.iloc[:, k],
+#                         hue='value', data=df, ax=ax1[i][j])
+#             k += 1
+# plt.show()
 
-fig, axes = plt.subplots(11, 11, figsize=(50, 50))
-for i in range(11):
-    for j in range(11):
-        axes[i, j].scatter(df.iloc[:, i], df.iloc[:, j], c=df.value)
-        axes[i, j].set_xlabel(df.columns[i])
-        axes[i, j].set_ylabel(df.columns[j])
-        axes[i, j].legend(df.value)
-plt.show()
+# fig, axes = plt.subplots(11, 11, figsize=(50, 50))
+# for i in range(11):
+#     for j in range(11):
+#         axes[i, j].scatter(df.iloc[:, i], df.iloc[:, j], c=df.value)
+#         axes[i, j].set_xlabel(df.columns[i])
+#         axes[i, j].set_ylabel(df.columns[j])
+#         axes[i, j].legend(df.value)
+# plt.show()
 
-# Now seperate the dataset as response variable and feature variabes
-Xb = df.drop(['quality', 'value'], axis=1)
-# y = pd.DataFrame(data['value'])
-yb = df['value']
+# # Now seperate the dataset as response variable and feature variabes
+# Xb = df.drop(['quality', 'value'], axis=1)
+# # y = pd.DataFrame(data['value'])
+# yb = df['value']
 
-# Normalization
-Xb_t = normalization(Xb)
-print("X_t:", Xb_t.shape)
+# # Normalization
+# Xb_t = normalization(Xb)
+# print("X_t:", Xb_t.shape)
 
-# Train and Test splitting of data
-xb_train, xb_test, yb_train, yb_test = train_test(Xb_t, yb)
+# # Train and Test splitting of data
+# xb_train, xb_test, yb_train, yb_test = train_test(Xb_t, yb)
 
-lrb = LogisticRegression()
-apply_classification('Logistic_Regression', lrb,
-                     xb_train, xb_test, yb_train, yb_test)
+# lrb = LogisticRegression()
+# apply_classification('Logistic_Regression', lrb,
+#                      xb_train, xb_test, yb_train, yb_test)
 
-svmb = SVC()
-apply_classification('SVM', svmb, xb_train, xb_test, yb_train, yb_test)
+# svmb = SVC()
+# apply_classification('SVM', svmb, xb_train, xb_test, yb_train, yb_test)
 
-dtb = DecisionTreeClassifier()
-dtb_clf = apply_classification(
-    'Decision_Tree', dtb, xb_train, xb_test, yb_train, yb_test)
+# dtb = DecisionTreeClassifier()
+# dtb_clf = apply_classification(
+#     'Decision_Tree', dtb, xb_train, xb_test, yb_train, yb_test)
 
-rfb = RandomForestClassifier(n_estimators=100)
-apply_classification('Random_Forest', rfb, xb_train,
-                     xb_test, yb_train, yb_test)
+# rfb = RandomForestClassifier(n_estimators=100)
+# apply_classification('Random_Forest', rfb, xb_train,
+#                      xb_test, yb_train, yb_test)
 
-# Plot the decision tree
-dot_data = export_graphviz(dtb_clf, out_file=None,
-                           filled=True, rounded=True, special_characters=True)
-graph = graphviz.Source(dot_data)
-graph
+# # Plot the decision tree
+# dot_data = export_graphviz(dtb_clf, out_file=None,
+#                            filled=True, rounded=True, special_characters=True)
+# graph = graphviz.Source(dot_data)
+# graph
 
-##### Categorical analysis ##############################################
+# ##### Categorical analysis ##############################################
 
-# Add a new feature according to mean of the quality
-# Good wine represented by 2, average 1, and bad wine represented by 0
-df['value'] = ""
-df['value'] = [2 if each > 6 else 1 if (
-    (each > 4) and (each < 7)) else 0 for each in data['quality']]
+# # Add a new feature according to mean of the quality
+# # Good wine represented by 2, average 1, and bad wine represented by 0
+# df['value'] = ""
+# df['value'] = [2 if each > 6 else 1 if (
+#     (each > 4) and (each < 7)) else 0 for each in df['quality']]
 
-print("Good Wine Class:", df[df['value'] == 2].shape)
-print("Average Wine Class:", df[df['value'] == 1].shape)
-print("Bad Wine Class:", df[df['value'] == 0].shape)
+# print("Good Wine Class:", df[df['value'] == 2].shape)
+# print("Average Wine Class:", df[df['value'] == 1].shape)
+# print("Bad Wine Class:", df[df['value'] == 0].shape)
 
-# Check the outliers for each feature with respect to output value
-fig, ax1 = plt.subplots(4, 3, figsize=(22, 16))
-k = 0
-for i in range(4):
-    for j in range(3):
-        if k != 11:
-            sns.boxplot(x='value', y=df.iloc[:, k], data=df, ax=ax1[i][j])
-            k += 1
-plt.show()
+# # Check the outliers for each feature with respect to output value
+# fig, ax1 = plt.subplots(4, 3, figsize=(22, 16))
+# k = 0
+# for i in range(4):
+#     for j in range(3):
+#         if k != 11:
+#             sns.boxplot(x='value', y=df.iloc[:, k], data=df, ax=ax1[i][j])
+#             k += 1
+# plt.show()
 
-# Categorical distribution plots:
-fig, ax1 = plt.subplots(4, 3, figsize=(22, 16))
-k = 0
-for i in range(4):
-    for j in range(3):
-        if k != 11:
-            sns.barplot(x="value", y=df.iloc[:, k],
-                        hue='value', data=df, ax=ax1[i][j])
-            k += 1
-plt.show()
+# # Categorical distribution plots:
+# fig, ax1 = plt.subplots(4, 3, figsize=(22, 16))
+# k = 0
+# for i in range(4):
+#     for j in range(3):
+#         if k != 11:
+#             sns.barplot(x="value", y=df.iloc[:, k],
+#                         hue='value', data=df, ax=ax1[i][j])
+#             k += 1
+# plt.show()
 
-fig, axes = plt.subplots(11, 11, figsize=(50, 50))
-for i in range(11):
-    for j in range(11):
-        axes[i, j].scatter(df.iloc[:, i], df.iloc[:, j], c=df.value)
-        axes[i, j].set_xlabel(df.columns[i])
-        axes[i, j].set_ylabel(df.columns[j])
-        axes[i, j].legend(df.value)
-plt.show()
+# fig, axes = plt.subplots(11, 11, figsize=(50, 50))
+# for i in range(11):
+#     for j in range(11):
+#         axes[i, j].scatter(df.iloc[:, i], df.iloc[:, j], c=df.value)
+#         axes[i, j].set_xlabel(df.columns[i])
+#         axes[i, j].set_ylabel(df.columns[j])
+#         axes[i, j].legend(df.value)
+# plt.show()
 
-# Now seperate the dataset as response variable and feature variabes
-X3 = df.drop(['quality', 'value'], axis=1)
-# y = pd.DataFrame(data['value'])
-y3 = df['value']
+# # Now seperate the dataset as response variable and feature variabes
+# X3 = df.drop(['quality', 'value'], axis=1)
+# # y = pd.DataFrame(data['value'])
+# y3 = df['value']
 
-# Normalization
-X3_t = normalization(X3)
-print("X_t:", X3_t.shape)
+# # Normalization
+# X3_t = normalization(X3)
+# print("X_t:", X3_t.shape)
 
-# Train and Test splitting of data
-x3_train, x3_test, y3_train, y3_test = train_test(X3_t, y3)
+# # Train and Test splitting of data
+# x3_train, x3_test, y3_train, y3_test = train_test(X3_t, y3)
 
-# Apply all previous classification algorithms
-lr3 = LogisticRegression()
-apply_classification('Logistic_Regression', lr3,
-                     x3_train, x3_test, y3_train, y3_test)
+# # Apply all previous classification algorithms
+# lr3 = LogisticRegression()
+# apply_classification('Logistic_Regression', lr3,
+#                      x3_train, x3_test, y3_train, y3_test)
 
-svm3 = SVC()
-apply_classification('SVM', svm3, x3_train, x3_test, y3_train, y3_test)
+# svm3 = SVC()
+# apply_classification('SVM', svm3, x3_train, x3_test, y3_train, y3_test)
 
-dt3 = DecisionTreeClassifier()
-dt3_clf = apply_classification(
-    'Decision_Tree', dt3, x3_train, x3_test, y3_train, y3_test)
+# dt3 = DecisionTreeClassifier()
+# dt3_clf = apply_classification(
+#     'Decision_Tree', dt3, x3_train, x3_test, y3_train, y3_test)
 
-rf3 = RandomForestClassifier(n_estimators=100)
-apply_classification('Random_Forest', rf3, x3_train,
-                     x3_test, y3_train, y3_test)
+# rf3 = RandomForestClassifier(n_estimators=100)
+# apply_classification('Random_Forest', rf3, x3_train,
+#                      x3_test, y3_train, y3_test)
 
-# Plot the decision tree
-dot_data = export_graphviz(dt3_clf, out_file=None,
-                           filled=True, rounded=True, special_characters=True)
-graph = graphviz.Source(dot_data)
-graph
+# # Plot the decision tree
+# dot_data = export_graphviz(dt3_clf, out_file=None,
+#                            filled=True, rounded=True, special_characters=True)
+# graph = graphviz.Source(dot_data)
+# graph

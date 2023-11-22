c2f95b5f1f6b968fd87b6223acc79e725e979ec2
jmccutchen <purplegrits@yahoo.com>
2023-11-21 19:52:47 -0500

    update

--- wine_quality.py
+++ wine_quality.py
@@ -17,11 +17,8 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import precision_recall_curve
 from sklearn.metrics import auc
 from sklearn.metrics import roc_curve
-from sklearn.linear_model import LinearRegression
 from sklearn.metrics import accuracy_score
 from sklearn.svm import SVC
-from six import StringIO
-from IPython.display import Image
 import matplotlib.pyplot as plt
 import seaborn as sns
 import warnings
@@ -84,118 +81,74 @@ print('Totals after cleaning: \n', df.shape)
 warnings.filterwarnings('ignore')
 
 # show histplot for each column
-# plt.figure(figsize=(15, 10))
-# plot_num = 1
-# for i in df.columns:
-#     ax = plt.subplot(4, 4, plot_num)
-#     sns.histplot(df[i], kde=True)
-#     plot_num += 1
-# plt.tight_layout()
-# plt.show()
+plt.figure(figsize=(15, 10))
+plot_num = 1
+for i in df.columns:
+    ax = plt.subplot(4, 4, plot_num)
+    sns.histplot(df[i], kde=True)
+    plot_num += 1
+plt.tight_layout()
+plt.show()
 
 # correlation map
 # -1 means: There is a negative relationship between dependent and independent variables .
 # 0 means: There is no relationship between dependent and independent variables .
 # 1 means: There is a positive relationship between dependent and independent variables .
 # According to this information, it can be made a good analysis about dataset and columns.
-# df.corr()
-# f, ax = plt.subplots(figsize=(10, 10))
-# sns.heatmap(df.corr(), annot=True, linewidths=.5, fmt=".2f", ax=ax)
-# plt.show()
+df.corr()
+f, ax = plt.subplots(figsize=(10, 10))
+sns.heatmap(df.corr(), annot=True, linewidths=.5, fmt=".2f", ax=ax)
+plt.show()
 
 # show multivariate analysis
-# sns.pairplot(df, hue='quality', height=3)
-# plt.tight_layout()
-# plt.show()
+sns.pairplot(df, hue='quality', height=3)
+plt.tight_layout()
+plt.show()
 
 # show scatterplot analysis
-# fig, axes = plt.subplots(11, 11, figsize=(15, 15))
-# for i in range(11):
-#     for j in range(11):
-#         axes[i, j].scatter(df.iloc[:, i], df.iloc[:, j], c=df.quality)
-#         axes[i, j].set_xlabel(df.columns[i])
-#         axes[i, j].set_ylabel(df.columns[j])
-#         axes[i, j].legend(df.quality)
-# plt.tight_layout()
-# plt.show()
+fig, axes = plt.subplots(11, 11, figsize=(50, 50))
+for i in range(11):
+    for j in range(11):
+        axes[i, j].scatter(df.iloc[:, i], df.iloc[:, j], c=df.quality)
+        axes[i, j].set_xlabel(df.columns[i])
+        axes[i, j].set_ylabel(df.columns[j])
+        axes[i, j].legend(df.quality)
+plt.tight_layout()
+plt.show()
 
-# g = sns.pairplot(df, hue="quality")
+g = sns.pairplot(df, hue="quality")
 
-# # distribution of quality rankings
-# sns.barplot(x=df['quality'].unique(), y=df['quality'].value_counts())
-# plt.xlabel("Quality Rankings")
-# plt.ylabel("Number of Wine")
-# plt.title("Distribution of Wine Quality Ratings")
-# plt.show()
-
-
-# # Check the outliers for each feature with respect to output value
-# fig, ax1 = plt.subplots(4, 3, figsize=(22, 16))
-# k = 0
-# for i in range(4):
-#     for j in range(3):
-#         if k != 12:
-#             sns.barplot(y=df.iloc[:, k], x='quality',
-#                         data=df, ax=ax1[i][j])
-#             k += 1
-# plt.tight_layout()
-# plt.show()
-
-# # Check the outliers for each feature with respect to output value
-# fig, ax1 = plt.subplots(4, 3, figsize=(22, 16))
-# k = 0
-# for i in range(4):
-#     for j in range(3):
-#         if k != 11:
-#             sns.boxplot(x='quality', y=df.iloc[:, k], data=df, ax=ax1[i][j])
-#             k += 1
-# plt.show()
-
-##################################  Models- Classification and Cross Validation ##################################
-
-# From Class
-
-# Predictors and target
-# x = df.drop('quality', axis=1)
-# y = df.quality
-# x_train, x_test, y_train, y_test = train_test_split(x, y)
-# sc = StandardScaler()
-# x_train = sc.fit_transform(x_train)
-# x_test = sc.fit_transform(x_test)
-
-# # Logistic Regression
-# lr = LogisticRegression()
-# lr.fit(x_train, y_train)
-# pred = lr.predict(x_test)
-# confusion_matrix_lr = confusion_matrix(y_test, pred)
-# print('\n\nConfusion Matrix LR: \n\n', confusion_matrix_lr)
-# print('\n\nLogistic Regression Classifier: \n\n',
-#       classification_report(y_test, pred))
+# distribution of quality rankings
+sns.barplot(x=df['quality'].unique(), y=df['quality'].value_counts())
+plt.xlabel("Quality Rankings")
+plt.ylabel("Number of Wine")
+plt.title("Distribution of Wine Quality Ratings")
+plt.show()
 
 
-# # Random Forest
-# xgb = RandomForestClassifier()
-# xgb.fit(x_train, y_train)
-# pred = xgb.predict(x_test)
-# print('Random Forest Classifier: \n\n', classification_report(y_test, pred))
+# Check the outliers for each feature with respect to output value
+fig, ax1 = plt.subplots(4, 3, figsize=(22, 16))
+k = 0
+for i in range(4):
+    for j in range(3):
+        if k != 12:
+            sns.barplot(y=df.iloc[:, k], x='quality',
+                        data=df, ax=ax1[i][j])
+            k += 1
+plt.tight_layout()
+plt.show()
 
-# # Linear Regression
-# regr = LinearRegression().fit(x_train, y_train)
-# y_hat = regr.predict(x_test)
-# y_hat[y_hat > 10] = 10
-# y_hat[y_hat < 0] = 0
-# y_hat = y_hat.round().astype("int")
-# print("Linear Regression Accuracy Score: {accur:.2f} \n\n".format(
-#     accur=accuracy_score(y_test, y_hat)))
+# Check the outliers for each feature with respect to output value
+fig, ax1 = plt.subplots(4, 3, figsize=(22, 16))
+k = 0
+for i in range(4):
+    for j in range(3):
+        if k != 11:
+            sns.boxplot(x='quality', y=df.iloc[:, k], data=df, ax=ax1[i][j])
+            k += 1
+plt.tight_layout()
+plt.show()
 
-# # Decision Tree
-# dt = DecisionTreeClassifier(
-#     criterion="entropy", random_state=1).fit(x_train, y_train)
-# y_hat = dt.predict(x_test)
-# print("Decision Tree Accuracy Score: {accur:.2f} \n\n".format(
-#     accur=accuracy_score(y_test, y_hat)))
-
-# end from class
 
 ##################################  Models- Classification and Cross Validation ##################################
 
@@ -376,11 +329,6 @@ print('running LR classification')
 apply_classification('Logistic_Regression', lr,
                      x_train, x_test, y_train, y_test)
 
-
-# svm = SVC(gamma='auto', verbose=True)
-# print('running SVM classification')
-# apply_classification('SVM', svm, x_train, x_test, y_train, y_test)
-
 dt = DecisionTreeClassifier()
 dt.fit(x_train, y_train)
 y_pred_log = dt.predict(x_test)
@@ -394,7 +342,7 @@ dt_clf = apply_classification(
 dot_data = export_graphviz(dt_clf, out_file=None,
                            filled=True, rounded=True, special_characters=True)
 graph = graphviz.Source(dot_data)
-graph
+# graph.view()
 
 rf = RandomForestClassifier(n_estimators=100, verbose=True)
 rf.fit(x_train, y_train)
@@ -404,7 +352,7 @@ rf_precision, rf_recall, _ = precision_recall_curve(
 print('running RF classification')
 apply_classification('Random_Forest', rf, x_train, x_test, y_train, y_test)
 
-# Precision recall curves analysis
+# # Precision recall curves analysis
 
 no_skill = len(y_test[y_test == 1]) / len(y_test)
 
@@ -447,74 +395,131 @@ plt.show()
 ##### Binary dataset analysis ##############################################
 # Add a new feature according to mean of the quality
 # Good wine represented by 1, bad wine represented by 0
-# df['value'] = ""
-# df['value'] = [1 if each > 5 else 0 for each in df['quality']]
+df['value'] = ""
+df['value'] = [1 if each > 5 else 0 for each in df['quality']]
 
-# print("Good Wine Class:", df[df['value'] == 1].shape)
-# print("Bad Wine Class:", df[df['value'] == 0].shape)
+print("Good Wine Class:", df[df['value'] == 1].shape)
+print("Bad Wine Class:", df[df['value'] == 0].shape)
 
-# # Check the outliers for each feature with respect to output value
-# fig, ax1 = plt.subplots(4, 3, figsize=(22, 16))
-# k = 0
-# for i in range(4):
-#     for j in range(3):
-#         if k != 11:
-#             sns.boxplot(x='value', y=df.iloc[:, k], data=df, ax=ax1[i][j])
-#             k += 1
-# plt.show()
+# Check the outliers for each feature with respect to output value
+fig, ax1 = plt.subplots(4, 3, figsize=(22, 16))
+k = 0
+for i in range(4):
+    for j in range(3):
+        if k != 11:
+            sns.boxplot(x='value', y=df.iloc[:, k], data=df, ax=ax1[i][j])
+            k += 1
+plt.tight_layout()
+plt.show()
 
-# # Categorical distribution plots:
-# fig, ax1 = plt.subplots(4, 3, figsize=(22, 16))
-# k = 0
-# for i in range(4):
-#     for j in range(3):
-#         if k != 11:
-#             sns.barplot(x="value", y=df.iloc[:, k],
-#                         hue='value', data=df, ax=ax1[i][j])
-#             k += 1
-# plt.show()
+# Categorical distribution plots:
+fig, ax1 = plt.subplots(4, 3, figsize=(22, 16))
+k = 0
+for i in range(4):
+    for j in range(3):
+        if k != 11:
+            sns.barplot(x="value", y=df.iloc[:, k],
+                        hue='value', data=df, ax=ax1[i][j])
+            k += 1
+plt.tight_layout()
+plt.show()
 
-# fig, axes = plt.subplots(11, 11, figsize=(50, 50))
-# for i in range(11):
-#     for j in range(11):
-#         axes[i, j].scatter(df.iloc[:, i], df.iloc[:, j], c=df.value)
-#         axes[i, j].set_xlabel(df.columns[i])
-#         axes[i, j].set_ylabel(df.columns[j])
-#         axes[i, j].legend(df.value)
-# plt.show()
+fig, axes = plt.subplots(11, 11, figsize=(50, 50))
+for i in range(11):
+    for j in range(11):
+        axes[i, j].scatter(df.iloc[:, i], df.iloc[:, j], c=df.value)
+        axes[i, j].set_xlabel(df.columns[i])
+        axes[i, j].set_ylabel(df.columns[j])
+        axes[i, j].legend(df.value)
+        leg = plt.legend(loc='upper center')
+# plt.tight_lay out()
+plt.show()
 
-# # Now seperate the dataset as response variable and feature variabes
-# Xb = df.drop(['quality', 'value'], axis=1)
-# # y = pd.DataFrame(data['value'])
-# yb = df['value']
+# Now seperate the dataset as response variable and feature variabes
+Xb = df.drop(['quality', 'value'], axis=1)
+# y = pd.DataFrame(data['value'])
+yb = df['value']
 
-# # Normalization
-# Xb_t = normalization(Xb)
-# print("X_t:", Xb_t.shape)
+# Normalization
+Xb_t = normalization(Xb)
+print("X_t:", Xb_t.shape)
 
-# # Train and Test splitting of data
-# xb_train, xb_test, yb_train, yb_test = train_test(Xb_t, yb)
+# Train and Test splitting of data
+xb_train, xb_test, yb_train, yb_test = train_test(Xb_t, yb)
 
-# lrb = LogisticRegression()
-# apply_classification('Logistic_Regression', lrb,
-#                      xb_train, xb_test, yb_train, yb_test)
+lr = LogisticRegression()
+lr.fit(x_train, y_train)
+y_pred_log = lr.predict(x_test)
+lr_precision, lr_recall, _ = precision_recall_curve(
+    y_test, y_pred_log, pos_label=1)
+print('running LR binary classification')
+apply_classification('Logistic_Regression', lr,
+                     xb_train, xb_test, yb_train, yb_test)
 
-# svmb = SVC()
-# apply_classification('SVM', svmb, xb_train, xb_test, yb_train, yb_test)
+dt = DecisionTreeClassifier()
+dt.fit(x_train, y_train)
+y_pred_log = dt.predict(x_test)
+dt_precision, dt_recall, _ = precision_recall_curve(
+    y_test, y_pred_log, pos_label=1)
+print('running DT binary classification')
+dtb_clf = apply_classification(
+    'Decision_Tree', dt, xb_train, xb_test, yb_train, yb_test)
 
-# dtb = DecisionTreeClassifier()
-# dtb_clf = apply_classification(
-#     'Decision_Tree', dtb, xb_train, xb_test, yb_train, yb_test)
+rf = RandomForestClassifier(n_estimators=100)
+rf.fit(x_train, y_train)
+y_pred_log = rf.predict(x_test)
+rf_precision, rf_recall, _ = precision_recall_curve(
+    y_test, y_pred_log, pos_label=1)
+print('running RF binary classification')
+apply_classification('Random_Forest', rf, xb_train,
+                     xb_test, yb_train, yb_test)
 
-# rfb = RandomForestClassifier(n_estimators=100)
-# apply_classification('Random_Forest', rfb, xb_train,
-#                      xb_test, yb_train, yb_test)
+# Plot the decision tree
+dot_data = export_graphviz(dtb_clf, out_file=None,
+                           filled=True, rounded=True, special_characters=True)
+graph = graphviz.Source(dot_data)
+# graph.view()
 
-# # Plot the decision tree
-# dot_data = export_graphviz(dtb_clf, out_file=None,
-#                            filled=True, rounded=True, special_characters=True)
-# graph = graphviz.Source(dot_data)
-# graph
+# Precision recall curves analysis
+
+no_skill = len(y_test[y_test == 1]) / len(y_test)
+
+t = np.arange(0, 110, 10)
+fig, ax = plt.subplots(figsize=(10, 6))
+ax.plot(dt_recall, dt_precision, linestyle='--',
+        color='b',  lw=2.0, label='DT')
+ax.plot(lr_recall, lr_precision, linestyle='--',
+        color='y',  lw=2.0, label='LR')
+ax.plot(rf_recall, rf_precision, linestyle='--',
+        color='m',  lw=2.0, label='RF')
+
+ax.plot([0, 1], [no_skill, no_skill], marker='.',
+        color='g', lw=2.0, label='No skill')
+
+plt.xlabel('Recall', fontsize=18)
+plt.ylabel('Precision', fontsize=18)
+plt.title('Precision-Recall curve', fontsize=18)
+plt.legend(loc='best', fontsize=18)
+ax.grid(True)
+ticklines = ax.get_xticklines() + ax.get_yticklines()
+gridlines = ax.get_xgridlines()
+ticklabels = ax.get_xticklabels() + ax.get_yticklabels()
+
+for line in ticklines:
+    line.set_linewidth(3)
+
+for line in gridlines:
+    line.set_linestyle('-')
+
+for line in gridlines:
+    line.set_linestyle('-')
+
+for label in ticklabels:
+    label.set_color('black')
+    label.set_fontsize('large')
+
+plt.show()
+
 
 # ##### Categorical analysis ##############################################
 
@@ -590,4 +595,4 @@ plt.show()
 # dot_data = export_graphviz(dt3_clf, out_file=None,
 #                            filled=True, rounded=True, special_characters=True)
 # graph = graphviz.Source(dot_data)
-# graph
+# print(graph)
